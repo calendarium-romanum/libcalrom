de7a2a96c799e8398ebe712fb6124d50126790e0
Jakub Pavlík <severus@post.cz>
2017-11-19 14:18:32 +0100

    DRY tests

--- tests/calrom_test.c
+++ tests/calrom_test.c
@@ -2,130 +2,129 @@
  * Tests of the library's public interface
  */
 
+#include <stdlib.h>
+
 #include "../calrom.h"
 
-void test_day_advent_first_sunday(void)
-{
+typedef struct {
   CRCalendar calendar;
   CRSanctorale sanctorale;
   CRDay day;
-
   GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 27, 11, 2016);
+} CalRomTestFixture;
 
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+static CalRomTestFixture* build_fixture(CRLiturgicalYear year)
+{
+  // allocate
+  CalRomTestFixture *fixture = malloc(sizeof(CalRomTestFixture));
+  // initialize
+  calrom_build_calendar(&(fixture->calendar), year, &(fixture->sanctorale));
+  g_date_clear(&(fixture->date), 1);
+
+  return fixture;
+}
+
+static void free_fixture(CalRomTestFixture *fixture)
+{
+  free(fixture);
+}
+
+
+
+void test_day_advent_first_sunday(void)
+{
+  CalRomTestFixture *fixture = build_fixture(2016);
+
+  g_date_set_dmy(&(fixture->date), 27, 11, 2016);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ADVENT);
-  g_assert_cmpint(day.season_info.week, ==, 1);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ADVENT);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_first_ferial(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 9, 1, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 9, 1, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 1);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_first_saturday(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 14, 1, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 14, 1, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 1);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_second_sunday(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 15, 1, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 15, 1, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 2); // because there is no 1st Sunday in the OT
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 2); // because there is no 1st Sunday in the OT
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_second_monday(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 16, 1, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 16, 1, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 2);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 2);
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_last_sunday(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 26, 11, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 26, 11, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 34);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 34);
+
+  free_fixture(fixture);
 }
 
 void test_day_ordinary_last_saturday(void)
 {
-  CRCalendar calendar;
-  CRSanctorale sanctorale;
-  CRDay day;
+  CalRomTestFixture *fixture = build_fixture(2016);
 
-  GDate date;
-  g_date_clear(&date, 1);
-  g_date_set_dmy(&date, 2, 12, 2017);
-
-  calrom_build_calendar(&calendar, 2016, &sanctorale);
-  int result = calrom_day(&day, &date, &calendar);
+  g_date_set_dmy(&(fixture->date), 2, 12, 2017);
+  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
 
   g_assert_cmpint(result, ==, 0);
-  g_assert(day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(day.season_info.week, ==, 34);
+  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
+  g_assert_cmpint(fixture->day.season_info.week, ==, 34);
+
+  free_fixture(fixture);
 }
