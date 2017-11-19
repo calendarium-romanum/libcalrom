ac8bfa4d8f65e71637315f0b9442f90303d0f115
Jakub Pavlík <severus@post.cz>
2017-11-19 15:35:38 +0100

    further DRY tests

--- tests/calrom_test.c
+++ tests/calrom_test.c
@@ -6,125 +6,99 @@
 
 #include "../calrom.h"
 
-typedef struct {
+static void successful_day_query_test(
+                                      CRLiturgicalYear liturgical_year,
+                                      GDateDay date_day,
+                                      GDateMonth date_month,
+                                      GDateYear date_year,
+                                      CRSeason expected_season,
+                                      short expected_week
+                                      )
+{
   CRCalendar calendar;
   CRSanctorale sanctorale;
   CRDay day;
   GDate date;
-} CalRomTestFixture;
 
-static CalRomTestFixture* build_fixture(CRLiturgicalYear year)
-{
-  // allocate
-  CalRomTestFixture *fixture = malloc(sizeof(CalRomTestFixture));
-  // initialize
-  calrom_build_calendar(&(fixture->calendar), year, &(fixture->sanctorale));
-  g_date_clear(&(fixture->date), 1);
+  calrom_build_calendar(&calendar, liturgical_year, &sanctorale);
+  g_date_clear(&date, 1);
 
-  return fixture;
-}
+  g_date_set_dmy(&date, date_day, date_month, date_year);
+  int result = calrom_day(&day, &date, &calendar);
 
-static void free_fixture(CalRomTestFixture *fixture)
-{
-  free(fixture);
+  g_assert_cmpint(result, ==, 0);
+  g_assert(day.season_info.season == expected_season);
+  g_assert_cmpint(day.season_info.week, ==, expected_week);
 }
 
 
 
 void test_day_advent_first_sunday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 27, 11, 2016);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ADVENT);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 27, 11, 2016,
+                            // expected
+                            CR_SEASON_ADVENT, 1
+                            );
 }
 
 void test_day_ordinary_first_ferial(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 9, 1, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 9, 1, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 1
+                            );
 }
 
 void test_day_ordinary_first_saturday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 14, 1, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 1);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 14, 1, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 1
+                            );
 }
 
 void test_day_ordinary_second_sunday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 15, 1, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 2); // because there is no 1st Sunday in the OT
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 15, 1, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 2 // because there is no 1st Sunday in the OT
+                            );
 }
 
 void test_day_ordinary_second_monday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 16, 1, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 2);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 16, 1, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 2
+                            );
 }
 
 void test_day_ordinary_last_sunday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 26, 11, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 34);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 26, 11, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 34
+                            );
 }
 
 void test_day_ordinary_last_saturday(void)
 {
-  CalRomTestFixture *fixture = build_fixture(2016);
-
-  g_date_set_dmy(&(fixture->date), 2, 12, 2017);
-  int result = calrom_day(&(fixture->day), &(fixture->date), &(fixture->calendar));
-
-  g_assert_cmpint(result, ==, 0);
-  g_assert(fixture->day.season_info.season == CR_SEASON_ORDINARY);
-  g_assert_cmpint(fixture->day.season_info.week, ==, 34);
-
-  free_fixture(fixture);
+  successful_day_query_test(
+                            // given
+                            2016, 2, 12, 2017,
+                            // expected
+                            CR_SEASON_ORDINARY, 34
+                            );
 }
